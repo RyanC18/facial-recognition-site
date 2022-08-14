08fb806333af19e0301d688b3d00957d709aabe2
Ryan <corke_ryan@yahoo.com>
2022-08-14 03:47:33 -0400

    added ImageLinkForm and Rank

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
     <div className="App">
      <Navigation />
      <Logo />
+     <Rank />
      <ImageLinkForm />
   {/*<FaceRecognition /> */}
     </div>
