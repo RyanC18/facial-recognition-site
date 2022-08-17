e5c3f42ee5838f700ef3564d89f63aa4afccba51
Ryan <corke_ryan@yahoo.com>
2022-08-17 03:45:51 -0400

    added input and button console responses

--- src/App.js
+++ src/App.js
@@ -5,16 +5,34 @@ import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render(){
   return (
     <div className="App">
      <Navigation />
      <Logo />
      <Rank />
-     <ImageLinkForm />
+     <ImageLinkForm 
+     onInputChange={this.onInputChange} 
+     onButtonSubmit={this.onButtonSubmit}/>
   {/*<FaceRecognition /> */}
     </div>
   );
 }
-
+}
 export default App;
